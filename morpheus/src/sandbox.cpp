d700951eb0e1e41d2c319c241b7d444dc8fd7e32
Accacio <raccacio@poli.ufrj.br>
2020-08-13 13:11:22 +0200

    add simple example

---
+++ morpheus/src/sandbox.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+#include "engine.hpp"
+
+class Sandbox : public Game {
+    public:
+        Sandbox() {
+        }
+
+        ~Sandbox(){
+
+        }
+};
+
+Game * newGame(){
+    return new Sandbox();
+}
